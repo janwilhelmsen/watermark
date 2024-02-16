1ecf68dfe5dbfd0e82285e890aedf7a0c1ffeac4
Jan Wilhelmsen <janwil@gmail.com>
2024-02-16 09:22:13 +0100

    Added thumbnail func

--- main.py
+++ main.py
@@ -4,39 +4,30 @@ from tkinter import *
 from PIL import Image,ImageTk, ImageDraw,ImageFont
 
 
-
-
-
-
-
-
-
-
-
 def main():
 
     img_list=[]
     img_data={}
 
     def upload():
+        if len(img_list) >0:
+            canvas.delete(img_list[0])
+            canvas.pack()
+            img_list.remove(img_list[0])
         filename = filedialog.askopenfilename()
         print (f"Uploading {filename}")
         image=ImageTk.PhotoImage(Image.open(filename))
         img_list.append(image)
         img_data["path"]=filename
-
         print (img_data)
         img_height=img_list[0].height()
-        img_width=img_list[0].width()
-        
-        canvas.create_image(200,200,image=img_list[0])
-        
+        img_width=img_list[0].width()   
+        canvas.create_image(200,200,image=img_list[0]) 
         print (f"size of picture is {img_height}x{img_width}")
         lbl_size.config(text=f"{img_width}x{img_height}")
         canvas.place(x=100,y=100,anchor="center")
         canvas.pack()
-        switch_data=[btn_rsz,btn_water]
-        print (switch_data)
+        switch_data=[btn_rsz,btn_water,btn_thumb,rdb_l,rdb_m,rdb_s]
         switch(*switch_data)
 
     def watermark():
@@ -61,18 +52,41 @@ def main():
 
 
     def resize():
+        sizes={"small":(640,480),"medium":(1024,768),"large":(2096,1536)}
         # Load the image
         image=Image.open(img_data["path"])
         # Resize the image in the given (width, height)
-        img=image.resize((450, 350))
+        img=image.resize(sizes[var_size.get()])
         # reload the image in seperate frame
         image=ImageTk.PhotoImage(img)
         img.show()
+    
+
+    def thumb():
+        # Load the image
+        image=Image.open(img_data["path"])
+        # Resize the image in the given (width, height)
+        image.thumbnail((100,100))
+        # reload the image in seperate frame
+        #image=ImageTk.PhotoImage(img)
+        image.show()
+        
         
     def switch(*args,**kwargs):
         print ("turning on Buttons after upload is finished")
         for item in args:
             item.config(state="active")
+
+    def rsz_size():
+    
+        return var_size.get()
+    
+
+
+    def save_pic(image,name):
+        print (name)
+        print (image)
+
 
             
 
@@ -90,11 +104,24 @@ def main():
     btn_rsz=Button(frm,text="Resize",command=resize,highlightbackground="grey",width=8,state="disabled")
     btn=Button(frm,text="Upload",command=upload,highlightbackground="grey",width=8)
     btn_water=Button(frm,text="WaterMark",command=watermark,highlightbackground="grey",width=8,state="disabled")
+    btn_thumb=Button(frm,text="Thumbnail",command=thumb,highlightbackground="grey",width=8,state="disabled")
     lbl_size=Label(frm,text="Size: ",highlightbackground="grey",bg="Grey",fg="black",font=("Arial",20))
     lbl_size.place(relx=0.0 ,rely=0.99,anchor="sw",)
     btn.place(x=25,y=40)
-    btn_rsz.place(x=25,y=80)
+    btn_rsz.place(x=25,y=480)
+    btn_thumb.place(x=25,y=80)
     btn_water.place(x=25,y=120)
+
+    var_size=StringVar()
+    var_size.set('small')
+    rdb_s=Radiobutton(frm,text="640x480",highlightbackground="grey",bg="grey",fg="black",state="disabled",variable=var_size,command=rsz_size,value='small')
+    rdb_m=Radiobutton(frm,text="1024x768",highlightbackground="grey",bg="grey",fg="black",state="disabled",variable=var_size,command=rsz_size,value='medium')
+    rdb_l=Radiobutton(frm,text="2096x1536",highlightbackground="grey",bg="grey",fg="black",state="disabled",variable=var_size,command=rsz_size,value='large')
+    rdb_s.place(x=25,y=520)
+    rdb_m.place(x=25,y=550)
+    rdb_l.place(x=25,y=580)
+
+    
     canvas=Canvas(frm2,width=500,height=500)
   
     
